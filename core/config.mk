f50578ad524b8eae97d515b3cc344a2b71abc650
Kry9toN <dhimasbagusprayoga@gmail.com>
2019-06-13 12:07:36 +0700

    Rebrand BeastROMs -> KomodOS Rom

--- core/config.mk
+++ core/config.mk
@@ -231,7 +231,7 @@ $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(CUSTOM_BUILD),)
-include vendor/beast/config/BoardConfigBeast.mk
+include vendor/komodo/config/BoardConfigKomodo.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1104,7 +1104,7 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/beast/sepolicy/common/sepolicy.mk)
+$(eval include device/komodo/sepolicy/common/sepolicy.mk)
 endif
 
 # Include any vendor specific config.mk file
@@ -1114,10 +1114,10 @@ endif
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
 # Rules for QCOM targets
--include $(TOPDIR)vendor/beast/build/core/qcom_target.mk
+-include $(TOPDIR)vendor/komodo/build/core/qcom_target.mk
 
 # Rules for MTK targets
--include $(TOPDIR)vendor/beast/build/core/mtk_target.mk
+-include $(TOPDIR)vendor/komodo/build/core/mtk_target.mk
 
 
 
